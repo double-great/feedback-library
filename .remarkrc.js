73514f098008953c164f5a05e7958051bf6737dd
Katy DeCorah <katydecorah@gmail.com>
2020-06-22 15:22:42 -0400

    update remark-frontmatter-validator

---
+++ .remarkrc.js
@@ -0,0 +1,26 @@
+module.exports.plugins = [
+  ["@double-great/remark-lint-alt-text"],
+  ["remark-frontmatter", ["yaml"]],
+  [
+    "remark-frontmatter-validator",
+    [
+      2,
+      {
+        title: {
+          type: "string",
+          required: true,
+        },
+        image: {
+          type: "string",
+          match: "^img/\\d\\d\\d\\d-\\d\\d-\\d\\d-.*.(png|jpg)$",
+          required: true,
+        },
+        categories: {
+          type: "array",
+          oneOf: ["documentation", "product"],
+          required: true,
+        },
+      },
+    ],
+  ],
+];
